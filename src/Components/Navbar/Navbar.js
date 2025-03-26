e381243440fb5b30d09f376f9703a238b9d5ee9d
sameeh <sameehmarhoun@hotmail.com>
2025-03-25 20:08:38 -0400

    create components

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,31 @@
+// Import necessary modules from React library
+import React, { useEffect } from 'react';
+
+// Import components for routing from react-router-dom library
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Import custom Navbar component
+import Navbar from './Components/Navbar/Navbar';
+
+// Function component for the main App
+function App() {
+
+  // Render the main App component
+  return (
+    <div className="App">
+        {/* Set up BrowserRouter for routing */}
+        <BrowserRouter>
+          {/* Display the Navbar component */}
+          <Navbar/>
+
+          {/* Set up the Routes for different pages */}
+          <Routes>
+            {/* Define individual Route components for different pages */}
+          </Routes>
+        </BrowserRouter>
+    </div>
+  );
+}
+
+// Export the App component as the default export
+export default App;
